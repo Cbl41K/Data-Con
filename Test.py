bf9873172b7ed31fbe3f780241b9f6826a3ee6c8
Vlad <109718413+g-vl-v@users.noreply.github.com>
2023-06-29 17:13:28 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+db_1 = pd.read_excel('np_database_1.xlsx')
+db_2 = pd.read_excel('np_database_2.xlsx')
+
+db_new = pd.concat([db_1, db_2])
+
+db_new.to_excel('np_database_3.xlsx', index=False)
