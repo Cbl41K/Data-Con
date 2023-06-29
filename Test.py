90c712c9f21a2bdd3c277e6dc5213ac7f98bb94a
Vlad <109718413+g-vl-v@users.noreply.github.com>
2023-06-29 19:05:25 +0300

    Test.py

--- Test.py
+++ Test.py
@@ -5,14 +5,24 @@ import numpy as np
 db_1 = pd.read_excel('database1.xlsx')
 db_2 = pd.read_excel('database2.xlsx')
 
+"""Удаление лишнего"""
+db_1 = db_1.drop(columns = ['Unnamed: 2', '№'])
+db_2 = db_2.drop(columns = ['Unnamed: 0', 'No', 'Unnamed: 4' ] )
+
 """Изменение значений в столбце Surface_Charge"""
 db_1['Surface_Charge'] = np.where(db_1['Surface_Charge'] > 0, 'Positive',
                        np.where(db_1['Surface_Charge'] < 0, 'Negative',
                         np.where(db_1['Surface_Charge'] == 0, 'Neutral', 'Unknown')))
 
+"""Изменение значений в столбце Cell age: embryonic (E), Adult (A)"""
+db_2['Cell age: embryonic (E), Adult (A)'] = np.where(db_2['Cell age: embryonic (E), Adult (A)'] == 'Adult', 'A',
+                       np.where(db_2['Cell age: embryonic (E), Adult (A)'] == 'Embryonic', 'E',
+                        np.where(db_2['Cell age: embryonic (E), Adult (A)'] == 'Fetus', 'E', 'Unknown')))
+
 """Исправление отрицательной выживаемости"""
 db_1['Cell_Viability (%)'] = np.where(db_1['Cell_Viability (%)'] < 0, 0, db_1['Cell_Viability (%)'])
 db_2['Cell_Viability (%)'] = np.where(db_2['Cell_Viability (%)'] < 0, 0, db_2['Cell_Viability (%)'])
+
 
 """Объединение и сохранение"""
 db_new = pd.concat([db_1, db_2])
