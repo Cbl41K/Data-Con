10d8cca996fa2abda10857b1b109610f12f51550
Vlad <109718413+g-vl-v@users.noreply.github.com>
2023-06-29 18:20:24 +0300

    Test.py

--- Test.py
+++ Test.py
@@ -1,8 +1,15 @@
 import pandas as pd
+import numpy as np
 
+"""Чтение баз"""
 db_1 = pd.read_excel('np_database_1.xlsx')
 db_2 = pd.read_excel('np_database_2.xlsx')
 
-db_new = pd.concat([db_1, db_2])
+"""Изменение значений в столбце Surface_Charge"""
+db_1['Surface_Charge'] = np.where(db_1['Surface_Charge'] > 0, 'Positive',
+                       np.where(db_1['Surface_Charge'] < 0, 'Negative', 'Neutral'))
 
+
+"""Объединение и сохранение"""
+db_new = pd.concat([db_1, db_2])
 db_new.to_csv('np_database_3.csv', index=False)
