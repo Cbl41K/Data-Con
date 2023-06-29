1c2c05b68b76a08535f93d13683845ceeb146718
Vlad <109718413+g-vl-v@users.noreply.github.com>
2023-06-29 19:02:24 +0300

    Test.py

--- Test.py
+++ Test.py
@@ -10,6 +10,9 @@ db_1['Surface_Charge'] = np.where(db_1['Surface_Charge'] > 0, 'Positive',
                        np.where(db_1['Surface_Charge'] < 0, 'Negative',
                         np.where(db_1['Surface_Charge'] == 0, 'Neutral', 'Unknown')))
 
+"""Исправление отрицательной выживаемости"""
+db_1['Cell_Viability (%)'] = np.where(db_1['Cell_Viability (%)'] < 0, 0, db_1['Cell_Viability (%)'])
+db_2['Cell_Viability (%)'] = np.where(db_2['Cell_Viability (%)'] < 0, 0, db_2['Cell_Viability (%)'])
 
 """Объединение и сохранение"""
 db_new = pd.concat([db_1, db_2])
